707a3c21b9c096cf95bf1e0aa1a40e126cfc4e5f
Milos Milovanovic <milos.silni@gmail.com>
2011-09-06 15:15:58 +0200

    #1055 - small gix

--- lib/model/sfSimpleForumPost.php
+++ lib/model/sfSimpleForumPost.php
@@ -20,45 +20,4 @@ class sfSimpleForumPost extends PluginsfSimpleForumPost
 
         parent::delete($con, $preserveTopic);
     }
-    
-    public function sendEmailNotifications() 
-    {
-    	$user_emails = array();
-    	$user_emails[] = sfConfig::get('app_appflower_forum_notification_email');
-    	
-    	$c = new Criteria();
-    	$c->add(sfSimpleForumPostPeer::TOPIC_ID, $this->getTopicId());
-    	$posts = sfSimpleForumPostPeer::doSelect($c);
-    	
-    	foreach ($posts as $post) 
-    	{
-    		if ($post->getUserId())
-    		{
-    			$user_emails[] = $post->getUser()->getUsername();
-    		} else if ($post->getAuthorEmail()!='') {
-    			$user_emails[] = $post->getAuthorEmail();
-    		}
-    	}
-    	
-    	$user_emails = array_unique($user_emails);
-    	
-    	$configuration = sfProjectConfiguration::getActive();
-    	$configuration->loadHelpers('Partial'); 
-    	foreach ($user_emails as $email)
-    	{
-            if ($email) {
-            	
-                $parameters = array(
-                    'email'   => $email,
-                    'subject' => 'Forum notification',
-                    'from'    => 'Appflower Team',
-                    'post'    => $this,
-                    'rand'    => md5(time())
-                );
-                afAutomailerHelper::saveMail('mail', 'forumNotification', $parameters);
-                
-            }
-    	}
-    	
-    }
 }
